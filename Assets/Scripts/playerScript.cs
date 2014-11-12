ba729bb4a2af09468ed38f4451afacd365ea047a
sonimus <soniaparrilla4a@gmail.com>
2014-11-12 16:39:13 +0100

    Disparo, andar y pararse.
    Arreglados, falta bala.

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -8,30 +8,51 @@ public class playerScript : MonoBehaviour {
 
 
 	
-	void Start () {
+	private Animator animator;
 	
-		//animator = GetComponent<Animator> ();
+	// Use this for initialization
+	void Start () {
+		
+		animator = GetComponent<Animator> ();
+		
+		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		
 		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
 		var forceX = 0f;
 		var forceY = 0f;
-
-		//Calculate Velocity X
-		if (Input.GetKey ("right")) {
-						if (absVelX < maxVelocity.x)
-								forceX = speed;
-						this.transform.localScale = new Vector3 (1, 1, 1);
-				} else if (Input.GetKey ("left")) {
-						if (absVelX < maxVelocity.x)
-								forceX = -speed;
-						this.transform.localScale = new Vector3 (-1, 1, 1);
-				}
-			rigidbody2D.AddForce (new Vector2(forceX, forceY));
-
-	}
 		
+		// Calculate Velocity X
+		if (Input.GetKey("right"))
+		{
+			if(rigidbody2D.velocity.x < 0){
+				rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+			}
+			if(absVelX < maxVelocity.x)
+				forceX = speed;
+			this.transform.localScale = new Vector3(1, 1, 1);
+			//linterna.transform.rotation = Quaternion.LookRotation(new Vector3(90,0,0));
+		}
+		else if (Input.GetKey("left"))
+		{
+			if(rigidbody2D.velocity.x > 0)
+				rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+			if (absVelX < maxVelocity.x)
+				forceX = -speed;
+			this.transform.localScale = new Vector3(-1, 1, 1);
+			//linterna.transform.rotation = Quaternion.LookRotation(new Vector3(-90,0,0));
+		}
+		if(absVelX > 0)
+			animator.SetFloat("Velocity",absVelX);
+		rigidbody2D.AddForce(new Vector2(forceX, forceY));
+		
+		if (Input.GetKey ("c")) {
+			animator.SetBool ("Fire", true);
+			
+		}else{
+			animator.SetBool ("Fire", false);
+		}
+	}
 }
