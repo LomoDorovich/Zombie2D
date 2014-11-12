323e791c7f3dbc819e8c489e294f405abb797c46
sonimus <soniaparrilla4a@gmail.com>
2014-11-12 18:48:07 +0100

    Bala puesta

---
+++ Assets/Scripts/Disparo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disparo : MonoBehaviour {
+
+	public GameObject bala;
+	public Transform puntoDisparo;
+
+
+	public void Fire(){
+		if (bala != null) {
+
+			var clone = Instantiate(bala, puntoDisparo.position,
+			                        Quaternion.identity)as GameObject;
+			clone.transform.localScale = transform.localScale;
+		} else {
+			Debug.Log("El jugador no tiene balas");
+		}
+	}
+}
